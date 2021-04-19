4915fa4f87ea13a47a8433cece8b5400d1c9e76d
Alphena <alphena@gmail.com>
2021-04-19 14:25:44 -0700

    update aggregate

--- routes/api.js
+++ routes/api.js
@@ -24,24 +24,41 @@ router.put("/workouts/:id", ({ body, params }, res) => {
         });
 })
 
-router.get("/workouts", (req, res) => {
-    Workout.find()
-    .then(data => {
-        res.json(data);
-    })
-    .catch(err => {
-        res.status(400).json(err);
-    });
-})
+router.get("/workouts", async function (req, res) {
+    try {
+      const data = await Workout.aggregate([
+        {
+          $addFields: {
+            totalDuration: {
+              $sum: "$exercises.duration",
+            },
+          },
+        },
+      ]);
+      res.json(data);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
+  });
 
-router.get("/workouts/range", (req, res) =>{
-    Workout.find()
-    .then(data => {
-        res.json(data);
-    })
-    .catch(err => {
-        res.status(400).json(err);
-    });
+
+router.get("/workouts/range", async function (req, res) {
+try {
+      const data = await Workout.aggregate([
+        {
+          $addFields: {
+            totalDuration: {
+              $sum: "$exercises.duration",
+            },
+          },
+        },
+      ]).sort({day:-1}).limit(7)
+      res.json(data);
+    } catch (err) {
+      console.log(err);
+      res.status(500).send(err);
+    }
 })
 
 module.exports = router;
